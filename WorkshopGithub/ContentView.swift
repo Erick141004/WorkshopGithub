7886faee25a6374ee9362d9c66e4e50c5abe8490
Erick Lima <erickcreimberg@gmail.com>
2024-10-10 17:26:19 -0300

    Initial Commit

---
+++ WorkshopGithub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkshopGithub
+//
+//  Created by ERICK COSTA REIMBERG DE LIMA on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkshopGithub/WorkshopGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopGithubApp.swift
+//  WorkshopGithub
+//
+//  Created by ERICK COSTA REIMBERG DE LIMA on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
